478bacf196b7d099a329220875d1b3b90c31c544
sanathk7 <122560253+sanathk7@users.noreply.github.com>
2025-04-08 17:46:09 +0530

    Update TestRunnerTest.java

--- src/test/java/runners/TestRunnerTest.java
+++ src/test/java/runners/TestRunnerTest.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-    features = "src/test/resources/Feature", // Adjust this if your feature files are in a different folder
+    features = "src/test/resources/Feature/LocalApi.feature", // Adjust this if your feature files are in a different folder
 
     glue = {"Stepdefination", "utils"}, // This should match the package of your step definitions
    plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports.json"}, // Generates reports
